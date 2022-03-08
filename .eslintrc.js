d2ea49475fbf3e045e497161e68d289fa08837d6
Fabien <fabien.webdev@gmail.com>
2022-03-08 19:15:46 +0100

    add base folders & files

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    // note you must disable the base rule as it can report incorrect errors
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error']
+  }
+}
---
+++ src/components/QrScanner/QrScanner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type QrScannerProps = {}
+
+export default function QrScanner (props: QrScannerProps) {
+  return (
+    <div>QrScanner</div>
+  )
+}
