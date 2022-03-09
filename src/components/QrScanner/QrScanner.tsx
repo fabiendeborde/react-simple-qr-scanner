0116bef74ebf1d0d824207d8ebe4bf4d99abadf0
Fabien <fabien.webdev@gmail.com>
2022-03-09 10:31:44 +0100

    add boilerplate files for testing purpose

--- src/components/QrScanner/QrScanner.tsx
+++ src/components/QrScanner/QrScanner.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './QrScanner.module.css'
 
 type QrScannerProps = {}
 
 export default function QrScanner (props: QrScannerProps) {
   return (
-    <div>QrScanner</div>
+    <div className={styles.container}>QrScanner component</div>
   )
 }
