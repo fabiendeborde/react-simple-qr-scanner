7d258bfd768ffeeb927d5699ebee898ceef8371f
Fabien <fabien.webdev@gmail.com>
2022-03-09 10:21:42 +0100

    add css & css module support

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
+import postcss from 'rollup-plugin-postcss'
 
 const packageJson = require('./package.json')
 
@@ -23,12 +24,14 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' })
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss()
     ]
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
+    plugins: [dts()],
+    external: [/\.css$/]
   }
 ]
